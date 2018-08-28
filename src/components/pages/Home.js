cd7174caca6222d5dc8d246f899411523b3a3cf7
Joseph Tomoyasu <JTomoyasu@gmail.com>
2018-08-28 15:59:36 -0700

    Updated Profile Picture

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,7 +5,7 @@ const Home = () => (
     <h1>Home Page</h1>
     <div className="row">
       <div className="col-sm-6">
-        <img src={require("../images/Profile-picture.png")} className="img-thumbnail" alt="uh oh" />
+        <img src={require("../images/Profile-picture2.png")} className="img-thumbnail" alt="uh oh" />
       </div>
       <div className="col-sm-6">
         My name is Joseph Tomoyasu and I am a web developer with a background in Environmental Studies and Geography and an interest in history.
